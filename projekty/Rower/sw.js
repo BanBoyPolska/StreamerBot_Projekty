d724bce0a86927810c7612e881fef20828d103f3
BanBoyPolska <72341922+BanBoyPolska@users.noreply.github.com>
2026-06-10 22:48:37 +0200

    Add files via upload

--- projekty/Rower/sw.js
+++ projekty/Rower/sw.js
@@ -9,7 +9,3 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./'))));
 });
-
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request).then(r => r || caches.match("./index.html"))))
-})
