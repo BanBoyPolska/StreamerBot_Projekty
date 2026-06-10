05618b864baa1b98420b14b1b952df79118fb5e7
BanBoyPolska <72341922+BanBoyPolska@users.noreply.github.com>
2026-06-10 21:23:33 +0200

    Add files via upload

---
+++ projekty/Rower/sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'bike-dashboard-v3';
+const ASSETS = ['./', './bike_dashboard_v2.html', './manifest.webmanifest', './icon.svg'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./'))));
+});
